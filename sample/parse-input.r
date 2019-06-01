77306cb0ac03cf23d1ef2674ba51b328002d13c3
Andrija Jovanović <ja.andrija@outlook.com>
2019-06-01 13:25:44 +0200

    useless r

--- sample/parse-input.r
+++ sample/parse-input.r
@@ -1,5 +1,18 @@
-library(rjson)
+library(jsonlite)
+library(data.table)
 
 lines <- readLines("dataset-sample.json")
+something <- t(sapply(lines, fromJSON))
 
-df <- as.data.frame(t(sapply(Lines, fromJSON)))
+v <- c()
+
+for (x in something) 
+	v <- append(v, list(class = x$device_class, mac = x$mac))
+
+dt <- data.table(v)
+
+head(dt, 3)
+
+ans <- dt[, .(.N), by = "v"]
+
+ans
