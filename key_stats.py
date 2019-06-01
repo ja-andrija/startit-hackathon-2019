acd7660037e15281f5593cc2c8f55b5c3685d4e5
unknown <vlvesic@outlook.com>
2019-06-01 18:35:03 +0200

    small fixes

--- key_stats.py
+++ key_stats.py
@@ -40,6 +40,45 @@ def get_octet_class_stats(json_path):
     return mac_octets
 
 
+def get_mdns_stats(json_path):
+    data = read_proper_json_from_file(json_path)
+    stats_dev_count = {}
+    stats_dev_to_mdns = {}
+    stats_mdns_to_dev = {}
+    for dp in data:
+        dev_class = dp["device_class"]
+        if dev_class not in stats_dev_count.keys():
+            stats_dev_count[dev_class] = {
+                "pres": 0,
+                "not": 0
+            }
+        if dev_class not in stats_dev_to_mdns.keys():
+            stats_dev_to_mdns[dev_class] = {}
+        if "mdns_services" in dp.keys():
+            stats_dev_count[dev_class]["pres"] += 1
+            for key in dp["mdns_services"]:
+                if key not in stats_mdns_to_dev.keys():
+                    stats_mdns_to_dev[key] = {}
+                if dev_class not in stats_mdns_to_dev[key]:
+                    stats_mdns_to_dev[key][dev_class] = 0
+                if key not in stats_dev_to_mdns[dev_class]:
+                    stats_dev_to_mdns[dev_class][key] = 0
+                stats_mdns_to_dev[key][dev_class] += 1
+                stats_dev_to_mdns[dev_class][key] +=1
+        else:
+            stats_dev_count[dev_class]["not"] += 1
+        
+        stats_dev_to_mdns_out = {}
+        for (key, val) in stats_dev_to_mdns.items():
+            stats_dev_to_mdns_out[key] = sorted(val.items(), key=itemgetter(1), reverse=True)
+        stats_mdns_to_dev_out = {}
+        for key, val in stats_mdns_to_dev.items():
+            stats_mdns_to_dev_out[key] = sorted(val.items(), key=itemgetter(1), reverse=True)
+        
+    return stats_dev_count, stats_dev_to_mdns_out, stats_mdns_to_dev_out
+    
+    
+
 def get_dhcp_stats(json_path, tag_name = "dhcp"):
     value1_name ="paramlist"
     value2_name = "classid"
@@ -101,41 +140,3 @@ def get_dhcp_stats(json_path, tag_name = "dhcp"):
     write_json_to_file(stat_tag_device, r'C:\Users\vr1\startit-hackathon-2019\key_stats_dhcp_tags_dev.json')
     write_json_to_file(stat_device_tag, r'C:\Users\vr1\startit-hackathon-2019\key_stats_dhcp_dev_tags.json')
     return stat_device_tag
-
-def get_mdns_stats(json_path):
-    data = read_proper_json_from_file(json_path)
-    stats_dev_count = {}
-    stats_dev_to_mdns = {}
-    stats_mdns_to_dev = {}
-    for dp in data:
-        dev_class = dp["device_class"]
-        if dev_class not in stats_dev_count.keys():
-            stats_dev_count[dev_class] = {
-                "pres": 0,
-                "not": 0
-            }
-        if dev_class not in stats_dev_to_mdns.keys():
-            stats_dev_to_mdns[dev_class] = {}
-        if "mdns_services" in dp.keys():
-            stats_dev_count[dev_class]["pres"] += 1
-            for key in dp["mdns_services"]:
-                if key not in stats_mdns_to_dev.keys():
-                    stats_mdns_to_dev[key] = {}
-                if dev_class not in stats_mdns_to_dev[key]:
-                    stats_mdns_to_dev[key][dev_class] = 0
-                if key not in stats_dev_to_mdns[dev_class]:
-                    stats_dev_to_mdns[dev_class][key] = 0
-                stats_mdns_to_dev[key][dev_class] += 1
-                stats_dev_to_mdns[dev_class][key] +=1
-        else:
-            stats_dev_count[dev_class]["not"] += 1
-        
-        stats_dev_to_mdns_out = {}
-        for (key, val) in stats_dev_to_mdns.items():
-            stats_dev_to_mdns_out[key] = sorted(val.items(), key=itemgetter(1), reverse=True)
-        stats_mdns_to_dev_out = {}
-        for key, val in stats_mdns_to_dev.items():
-            stats_mdns_to_dev_out[key] = sorted(val.items(), key=itemgetter(1), reverse=True)
-        
-    return stats_dev_count, stats_dev_to_mdns_out, stats_mdns_to_dev_out
-    
