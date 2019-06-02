4878c6cf361688a33717b19988b9f7f08154f1a7
Stanko Kovacevic <stanko_kovacevic@outlook.com>
2019-06-02 02:17:00 +0200

    model upgrated to 0.89

--- run_pipeline.py
+++ run_pipeline.py
@@ -82,3 +82,27 @@ test_ids = np.argmax(test_aggregate, axis=1)
 classes = models[0].classes_
 test_predictions = list(map(lambda x: classes[x], test_ids))
 print(f"Average f1s on unseen: {f1_score(Y_real, test_predictions, average='micro')}")
+
+# PART 6 PREPARE SUBMISSION
+def get_data_for_submitting():
+    df_test = util.load_data_to_dataframe('dataset/test.json')
+    test_feats, _, test_ids = get_feats_labels_ids(create_features(df_test))
+    return test_feats, test_ids
+
+def dump_for_submitting():
+    X_submit, test_ids = get_data_for_submitting()
+    print("predicting")
+    test_aggregate = np.zeros([len(X_submit.index), 13])
+    for i in range(k):
+        test_predict = models[i].predict_proba(X_submit)
+        test_aggregate += test_predict
+    test_aggregate = test_aggregate / k
+    class_ids = np.argmax(test_aggregate, axis=1)
+    classes = models[0].classes_
+    test_predictions = list(map(lambda x: classes[x], class_ids))
+
+    with open("out/subm.csv", 'w') as f:
+        f.write("Id,Predicted\n")
+        for i in range(len(test_predictions)):
+            f.write(f"{test_ids[i]},{test_predictions[i]}\n")
+dump_for_submitting()
